3fe612879d5eb3477e1715c872dc9cdd95ec025e
Dan <dan@silvercrk.com>
2016-01-30 12:10:20 -0800

    added villager and volcano

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -2,24 +2,36 @@
 using System.Collections;
 
 public class MouseController : MonoBehaviour {
+	public Villager villager;
+	public GameObject volcano;
+	public GameObject village;
+
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		
+		village = GameObject.Find("Village");
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate ()
+	{
+		
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit, 100))
-            {
-                Debug.Log("You clicked on: " + hit.transform.gameObject.name);
-            }
-        }
+			if (Physics.Raycast(ray, out hit, 100))
+			{
+				Debug.Log(hit.transform.gameObject.name);
+				if (hit.transform.gameObject.tag == "Volcano")
+				{
+					Instantiate(villager, village.transform.position, Quaternion.identity);
+					villager.SetVolcano(hit.transform.gameObject);
+				}
+			}
+		}
 	}
 }
