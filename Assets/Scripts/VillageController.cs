29d5084fcf996a9ec1cb2f406f42b65351fe5397
LordKawaii <lordkawaii@gmail.com>
2016-01-30 12:35:45 -0800

    Created village prefab and resource generation.

---
+++ Assets/Scripts/VillageController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VillageController : MonoBehaviour {
+
+    VillageUIContoller uiController;
+    int resources = 0;
+
+    public float spawnTimerLength = 1;
+    float spawnTimer;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        spawnTimer = Time.time + spawnTimerLength;
+
+        uiController = gameObject.GetComponent<VillageUIContoller>();
+        uiController.SetResources(resources);
+
+    }
+
+    void Update()
+    {
+        if (Time.time >= spawnTimer)
+        {
+            spawnTimer = Time.time + spawnTimerLength;
+            resources++;
+            uiController.SetResources(resources);
+        }
+    }
+}
