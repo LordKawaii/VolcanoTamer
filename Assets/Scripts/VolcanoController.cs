039464d62e335fa61bdd25963c3bfcd50f47749a
Dan <dan@silvercrk.com>
2016-01-31 14:31:36 -0800

    randomly kill villagers

--- Assets/Scripts/VolcanoController.cs
+++ Assets/Scripts/VolcanoController.cs
@@ -57,7 +57,10 @@ public class VolcanoController : MonoBehaviour {
 		}
 
         setSoundLevel();
-    }
+
+		if(!isTamed)
+			RandomlyKillVillager();
+	 }
 
 	void Erupt()
 	{
@@ -71,6 +74,17 @@ public class VolcanoController : MonoBehaviour {
 			VillagerCon villager = villagers[n].GetComponent<VillagerCon>();
 			villager.Kill();
 			villagers.RemoveAt(n);
+		}
+	}
+
+	void RandomlyKillVillager()
+	{
+		if(Random.value < 0.005 && villagers.Count > 0)
+		{
+			Debug.Log(Random.value);
+			VillagerCon villager = villagers[0].GetComponent<VillagerCon>();
+			villager.Kill();
+			villagers.RemoveAt(0);
 		}
 	}
 
